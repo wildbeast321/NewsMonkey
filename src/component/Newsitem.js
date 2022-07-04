42c36261f614d8f587b72725d142d4ecc4036dc8
Anirudh Acharya <anirudhacharya2001@gmail.com>
2022-07-04 22:02:52 +0530

    app structure created

---
+++ src/component/Newsitem.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export class Newsitem extends Component {
+  render() {
+    let { title, description, imgurl, newsurl } = this.props;
+    return (
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={imgurl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title"> {title}...</h5>
+          <p className="card-text">{description}...</p>
+          <a href={newsurl} target="_blank" className="btn btn-sm btn-primary">
+            Read More
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+export default Newsitem;
